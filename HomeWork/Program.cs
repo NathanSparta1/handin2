9fa617ae6fbdc6e1b0df312ef261745e4d625b8b
Nathan Ingleton <NIngleton@spartaglobal.com>
2019-10-28 18:33:42 +0000

    commited

---
+++ HomeWork/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        class Body
+        {
+            public int weight;
+            public string colour;
+        }
+        class Planet: Body
+        {
+            public int NumberOfInhabitants;
+
+        }
+
+        class SolarSystem
+        {
+            public int numberOfPlanets;
+            Planet Earth = new Planet();
+
+
+        }
+        class Sun: Body
+        {
+            public int Temperature;
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
